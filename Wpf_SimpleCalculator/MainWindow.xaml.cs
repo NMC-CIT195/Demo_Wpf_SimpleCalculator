b3e92028193589148392c3373f4caac8e6458a8d
John Velis <john@johnvelis.com>
2019-01-25 09:35:42 -0500

    Ver. 1.0

--- Wpf_SimpleCalculator/MainWindow.xaml.cs
+++ Wpf_SimpleCalculator/MainWindow.xaml.cs
@@ -20,7 +20,9 @@ namespace Wpf_SimpleCalculator
     /// </summary>
     public partial class MainWindow : Window
     {
-        string _units;
+        private enum Units { feet, meters };
+
+        Units _units;
 
         public MainWindow()
         {
@@ -30,7 +32,7 @@ namespace Wpf_SimpleCalculator
 
         private void InitializeWindow()
         {
-            TextBox_Length.BorderBrush = Brushes.Red;
+            TextBox_Length.Focus();
 
             UpdateUnits();
         }
@@ -46,31 +48,73 @@ namespace Wpf_SimpleCalculator
             helpWindow.ShowDialog();
         }
 
+        /// <summary>
+        /// calculate the volume and display the value
+        /// </summary>
         private void Button_Calculate_Click(object sender, RoutedEventArgs e)
         {
-            double answer = 0;
+            double volume = 0;
+            double shapeMultiplier = 1;
+            string shape = ComboBox_Shape.SelectionBoxItem as string;
+            string userFeedback;
 
-            if (ComboBox_Shape.SelectionBoxItem as string == "Rectangular Prism")
+            if (ValidInputs(out userFeedback))
             {
-                answer = 5;
-            }
+                volume =
+                    Convert.ToDouble(TextBox_Length.Text) *
+                    Convert.ToDouble(TextBox_Width.Text) *
+                    Convert.ToDouble(TextBox_Height.Text);
 
-            SolutionWindow solutionWindow = new SolutionWindow(answer);
-            solutionWindow.ShowDialog();
-        }
+                switch (shape)
+                {
+                    case "Rectangular Prism":
+                        shapeMultiplier = 1;
+                        break;
+                    case "Pyramidal Prism":
+                        shapeMultiplier = 1.0 / 3.0;
+                        break;
+                    default:
+                        shapeMultiplier = 1;
+                        break;
+                }
 
-        private void TextBox_Length_LostFocus(object sender, RoutedEventArgs e)
-        {
-            if (!double.TryParse(TextBox_Length.Text, out double temp))
-            {
-                TextBox_Length.Focus();
-                MessageBox.Show("You must enter a number");
-                TextBox_Length.Text = null;
+                volume *= shapeMultiplier;
+
+                TextBox_Volume.Text = String.Format("{0:0.##}", volume);
             }
             else
             {
-                TextBox_Length.BorderBrush = Brushes.Black;
+                MessageBox.Show(userFeedback);
             }
+        }
+
+        /// <summary>
+        /// validate all inputs and generate a user feedback message if necessary 
+        /// </summary>
+        /// <param name="userFeedback">user feedback message</param>
+        /// <returns>valid inputs status</returns>
+        private bool ValidInputs(out string userFeedback)
+        {
+            bool validInputs = true;
+            userFeedback = "";
+
+            if (!double.TryParse(TextBox_Length.Text, out double tempLength))
+            {
+                validInputs = false;
+                userFeedback += "It appears that the value entered for Length is not a valid number." + Environment.NewLine;
+            }
+            if (!double.TryParse(TextBox_Width.Text, out double tempWidth))
+            {
+                validInputs = false;
+                userFeedback += "It appears that the value entered for Width is not a valid number." + Environment.NewLine;
+            }
+            if (!double.TryParse(TextBox_Height.Text, out double tempHeight))
+            {
+                validInputs = false;
+                userFeedback += "It appears that the value entered for Height is not a valid number." + Environment.NewLine;
+            }
+
+            return validInputs;
         }
 
         private void RadioButton_Units_Checked(object sender, RoutedEventArgs e)
@@ -81,31 +125,24 @@ namespace Wpf_SimpleCalculator
             }
         }
 
+        /// <summary>
+        /// update units in labels
+        /// </summary>
         private void UpdateUnits()
         {
             if ((bool)RadioButton_Feet.IsChecked)
             {
-                _units = "feet";
+                _units = Units.feet;
             }
             else if ((bool)RadioButton_Meters.IsChecked)
             {
-                _units = "meters";
+                _units = Units.meters;
             }
 
             Label_Length.Content = "Length (" + _units + ")";
             Label_Width.Content = "Width (" + _units + ")";
             Label_Height.Content = "Height (" + _units + ")";
             Label_Volume.Content = "Volume (cubic " + _units + ")";
-        }
-
-        private void TextBox_Height_LostFocus(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void TextBox_Width_LostFocus(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
